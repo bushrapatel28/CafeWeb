a2ee5ed8e5e8bd982628a585b3b9e1db54f97654
bushrapatel28 <bpatel60@yahoo.com>
2025-06-13 18:40:08 +0000

    Add Suppliers

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import { useRoutes, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import MenuPage from "./pages/MenuPage";
+import CoffeeBeanSuppliers from "./components/CoffeeBeanSuppliers";
 import routes from "tempo-routes";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/menu" element={<MenuPage />} />
           <Route path="/menu/:category" element={<MenuPage />} />
+          <Route path="/suppliers" element={<CoffeeBeanSuppliers />} />
         </Routes>
         {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
       </>
